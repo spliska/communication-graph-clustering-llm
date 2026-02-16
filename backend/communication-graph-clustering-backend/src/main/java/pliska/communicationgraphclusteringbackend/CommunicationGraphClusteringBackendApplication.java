88938a2a7940eb745e037ac84a3228b4579a6f8b
spliska <spl@posteo.jp>
2026-02-16 20:52:25 +0100

    Added application class

---
+++ backend/communication-graph-clustering-backend/src/main/java/pliska/communicationgraphclusteringbackend/CommunicationGraphClusteringBackendApplication.java
@@ -0,0 +1,13 @@
+package pliska.communicationgraphclusteringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationGraphClusteringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunicationGraphClusteringBackendApplication.class, args);
+    }
+
+}
