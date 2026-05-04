ebe35e2b710b188be642b75289dbc3e0bec4a173
spliska <spl@posteo.jp>
2026-05-04 20:58:45 +0200

    Added queries to the email repository to get emails by sender, between 2 parties, findAllbyRecipient and to count emails with more than one recipient

--- backend/communication-graph-clustering-backend/src/main/java/pliska/communicationgraphclusteringbackend/db/email/EmailRepository.java
+++ backend/communication-graph-clustering-backend/src/main/java/pliska/communicationgraphclusteringbackend/db/email/EmailRepository.java
@@ -1,7 +1,29 @@
 package pliska.communicationgraphclusteringbackend.db.email;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmailRepository extends JpaRepository<EmailEntity, Long> {
+
     boolean existsByMessageId(String messageId);
+
+    @Query("SELECT e.messageId FROM EmailEntity e WHERE e.messageId IN :messageIds")
+    List<String> findAllMessageIdsByMessageIdIn(@Param("messageIds") List<String> messageIds);
+
+    EmailEntity getEmailEntityById(Long id);
+
+    @Query("SELECT e FROM EmailEntity e WHERE e.from = :senderEmailAddress")
+    List<EmailEntity> findAllBySender(@Param("senderEmailAddress") String senderEmailAddress);
+
+    @Query("SELECT e FROM EmailEntity e WHERE e.from = :senderEmailAddress AND e.toEmailAddress = :recipientEmailAddress ORDER BY e.date ASC")
+    List<EmailEntity> findAllBySenderAndRecipientOrderByDateAsc(@Param("senderEmailAddress") String senderEmailAddress, @Param("recipientEmailAddress") String recipientEmailAddress);
+
+    @Query("SELECT e FROM EmailEntity e WHERE e.toEmailAddress = :emailAddress")
+    List<EmailEntity> findAllByRecipient(@Param("emailAddress") String emailAddress);
+
+    @Query("SELECT COUNT(e) FROM EmailEntity e WHERE e.from = :senderEmail AND e.toEmailAddress LIKE '%,%'")
+    Integer countBySenderEmailsWithMoreThanOneRecipient(@Param("senderEmail") String senderEmail);
 }
